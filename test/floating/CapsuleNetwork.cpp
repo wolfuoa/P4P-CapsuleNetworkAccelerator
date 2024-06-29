6f0b34ee8d19c472e4b1ae243c53e18f538a00a0
wolfuoa <nwol626@aucklanduni.ac.nz>
2024-06-29 17:53:39 +1200

    feat: :sparkles: Refactored CapsuleNetwork top level

--- test/floating/CapsuleNetwork.cpp
+++ test/floating/CapsuleNetwork.cpp
@@ -1,24 +1,47 @@
 #include "CapsuleNetwork.h"
 
-#include "CapsuleLayer.h"
+#include <string>
+
+#include "DigitCaps.h"
 #include "PrimaryCaps.h"
 #include "ReLUConv1.h"
 
-void get_prediction(float image[IN_IMG_ROWS][IN_IMG_COLS], float prediction[DIGIT_CAPS_NUM_DIGITS])
+void get_prediction(float *image, float *weights, float *biases, float *prediction)
 {
 	// ---------------- ReLU Convolutional 2D Layer ----------------
-	hls::stream<float> stream_conv_s[CONV1_FILTERS];
+	float output_conv[CONV1_OUTPUT_LENGTH * CONV1_OUTPUT_WIDTH * CONV1_FILTERS];
+	float conv_weights[CONV1_KERNEL_ROWS * CONV1_KERNEL_COLS * CONV1_FILTERS];
+	float conv_biases[CONV1_FILTERS];
 
-	relu_conv_2d(image, stream_conv_s);
+	memcpy(conv_weights, (const float *)weights, CONV1_KERNEL_ROWS * CONV1_KERNEL_COLS * CONV1_FILTERS * sizeof(float));
+	memcpy(conv_biases, (const float *)biases, CONV1_FILTERS);
+
+	weights += CONV1_KERNEL_ROWS * CONV1_KERNEL_COLS * CONV1_FILTERS;
+	biases += CONV1_FILTERS;
+
+	relu_conv_2d(image, conv_weights, conv_biases, output_conv);
 	// ---------------- ReLU Convolutional 2D Layer ----------------
 
 	// ------------------- Primary Capsule Layer -------------------
-	hls::stream<float> stream_primary_caps_s;
+	uint32_t prim_dim = PRIMARY_CAPS_KERNEL_ROWS * PRIMARY_CAPS_KERNEL_COLS * PRIMARY_CAPS_KERNEL_DEPTH * PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES;
+	float output_prim[PRIMARY_CAPS_CONV_LENGTH * PRIMARY_CAPS_CONV_WIDTH * PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES];
+	float prim_weights[prim_dim];
+	float prim_biases[PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES];
 
-	process_features(stream_conv_s, stream_primary_caps_s);
+	memcpy(prim_weights, (const float *)weights, prim_dim * sizeof(float));
+	memcpy(prim_biases, (const float *)biases, PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES * sizeof(float));
+
+	weights += prim_dim;
+
+	process_features(output_conv, prim_weights, prim_biases, output_prim);
 	// ------------------- Primary Capsule Layer -------------------
 
 	// -------------------- Digit Capsule Layer --------------------
-	dynamic_routing(stream_primary_caps_s, prediction);
+	uint32_t digit_dim = DIGIT_CAPS_INPUT_CAPSULES * DIGIT_CAPS_INPUT_DIM_CAPSULE * DIGIT_CAPS_NUM_DIGITS * DIGIT_CAPS_DIM_CAPSULE;
+	float digit_weights[digit_dim];
+
+	memcpy(digit_weights, (const float *)weights, digit_dim * sizeof(float));
+
+	dynamic_routing(output_prim, digit_weights, prediction);
 	// -------------------- Digit Capsule Layer --------------------
 }
--- test/floating/CapsuleNetwork.h
+++ test/floating/CapsuleNetwork.h
@@ -6,6 +6,6 @@
 
 #include "constants.h"
 
-void get_prediction(float image[IN_IMG_ROWS][IN_IMG_COLS], float prediction[DIGIT_CAPS_NUM_DIGITS])
+void get_prediction(float *image, float *weights, float *biases, float *prediction);
 
 #endif
