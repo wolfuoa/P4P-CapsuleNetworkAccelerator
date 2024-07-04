b621804e9877e6d35da34d552f607f21de008fa8
wolfuoa <nwol626@aucklanduni.ac.nz>
2024-07-04 17:18:04 +1200

    feat: :sparkles: Added option to dump layers

--- test/util/testing_suite.h
+++ test/util/testing_suite.h
@@ -1,7 +1,8 @@
 #ifndef TESTING_SUITE_H
 #define TESTING_SUITE_H
 
-#define PRINT_DEBUG		   1
-#define NUM_IMAGES_TO_TEST 5
+#define PRINT_DEBUG		   0
+#define DUMP_LAYERS		   0
+#define NUM_IMAGES_TO_TEST 1
 
 #endif	// TESTING_SUITE_H
