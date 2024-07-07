5ee4d3054acd990713323eb68e679cefdcdfbc82
wolfuoa <nwol626@aucklanduni.ac.nz>
2024-07-07 12:57:25 +1200

    feat: :sparkles: Wrong file

--- vitishls/CapsuleNetwork.cpp
+++ vitishls/CapsuleNetwork.cpp
@@ -26,13 +26,14 @@
 
 void get_prediction(float *image, float *weights, float *biases, float *prediction)
 {
-    
-
+    #pragma HLS INTERFACE mode=m_axi port=image offset=slave bundle=gmem0 max_read_burst_length=256 max_write_burst_length=256 depth=784
+    #pragma HLS INTERFACE mode=m_axi port=weights offset=slave bundle=gmem1 max_read_burst_length=256 max_write_burst_length=256 depth=6803712
+    #pragma HLS INTERFACE mode=m_axi port=prediction offset=slave bundle=gmem2 max_read_burst_length=256 max_write_burst_length=256
 
 	// ---------------- ReLU Convolutional 2D Layer ----------------
-	float *output_conv = (float *)malloc(CONV1_OUTPUT_LENGTH * CONV1_OUTPUT_WIDTH * CONV1_FILTERS * sizeof(float));
-	float *conv_weights = (float *)malloc(CONV1_KERNEL_ROWS * CONV1_KERNEL_COLS * CONV1_FILTERS * sizeof(float));
-	float *conv_biases = (float *)malloc(CONV1_FILTERS * sizeof(float));
+	float output_conv[CONV1_OUTPUT_LENGTH * CONV1_OUTPUT_WIDTH * CONV1_FILTERS];
+	float conv_weights[CONV1_KERNEL_ROWS * CONV1_KERNEL_COLS * CONV1_FILTERS];
+	float conv_biases[CONV1_FILTERS];
 
 	memcpy(conv_weights, (const float *)weights, CONV1_KERNEL_ROWS * CONV1_KERNEL_COLS * CONV1_FILTERS * sizeof(float));
 	memcpy(conv_biases, (const float *)biases, CONV1_FILTERS);
@@ -45,9 +46,9 @@ void get_prediction(float *image, float *weights, float *biases, float *predicti
 
 	// ------------------- Primary Capsule Layer -------------------
 	uint32_t prim_dim = PRIMARY_CAPS_KERNEL_ROWS * PRIMARY_CAPS_KERNEL_COLS * PRIMARY_CAPS_KERNEL_DEPTH * PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES;
-	float *output_prim = (float *)malloc(PRIMARY_CAPS_CONV_LENGTH * PRIMARY_CAPS_CONV_WIDTH * PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES * sizeof(float));
-	float *prim_weights = (float *)malloc(prim_dim * sizeof(float));
-	float *prim_biases = (float *)malloc(PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES * sizeof(float));
+	float output_prim[PRIMARY_CAPS_CONV_LENGTH * PRIMARY_CAPS_CONV_WIDTH * PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES];
+	float prim_weights[PRIMARY_CAPS_KERNEL_ROWS * PRIMARY_CAPS_KERNEL_COLS * PRIMARY_CAPS_KERNEL_DEPTH * PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES];
+	float prim_biases[PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES];
 
 	memcpy(prim_weights, (const float *)weights, prim_dim * sizeof(float));
 	memcpy(prim_biases, (const float *)biases, PRIMARY_CAPS_CAPSULE_DIM * PRIMARY_CAPS_CAPSULES * sizeof(float));
@@ -59,7 +60,7 @@ void get_prediction(float *image, float *weights, float *biases, float *predicti
 
 	// -------------------- Digit Capsule Layer --------------------
 	uint32_t digit_dim = DIGIT_CAPS_INPUT_CAPSULES * DIGIT_CAPS_INPUT_DIM_CAPSULE * DIGIT_CAPS_NUM_DIGITS * DIGIT_CAPS_DIM_CAPSULE;
-	float *digit_weights = (float *)malloc(digit_dim * sizeof(float));
+	float digit_weights[DIGIT_CAPS_INPUT_CAPSULES * DIGIT_CAPS_INPUT_DIM_CAPSULE * DIGIT_CAPS_NUM_DIGITS * DIGIT_CAPS_DIM_CAPSULE];
 
 	memcpy(digit_weights, (const float *)weights, digit_dim * sizeof(float));
 
