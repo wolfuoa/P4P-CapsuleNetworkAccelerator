28e5baf47fa6a293151b39b7636dc23cc92564d0
wolfuoa <nwol626@aucklanduni.ac.nz>
2024-09-21 09:18:14 +1200

    feat: :sparkles: changed the partial/quantize code

--- deployment/quantizer.py
+++ deployment/quantizer.py
@@ -5,7 +5,6 @@ from tensorflow_model_optimization.quantization.keras import vitis_inspect, viti
 from keras import Model
 from capsulenet import *
 from capsulelayers import Length, Mask, CapsuleLayer, SquashLayer
-from createNoReconstructionModel import remove_reconstruction
 
 def quantize_model(file_path, save_path):
 
@@ -13,20 +12,15 @@ def quantize_model(file_path, save_path):
     (x_train, y_train), (x_test, y_test) = load_mnist()
 
     # load model to quantize
-    full_model = tf.keras.models.load_model(file_path, custom_objects={'Length': Length, 'CapsuleLayer': CapsuleLayer, 'SquashLayer': SquashLayer})
+    new_model = tf.keras.models.load_model(file_path, custom_objects={'Length': Length, 'CapsuleLayer': CapsuleLayer, 'SquashLayer': SquashLayer})
 
-    # Extract the all layers excluding digitCaps
-    partial_model = Model(inputs=full_model.input, outputs=full_model.layers[4].output)
+    # quantize model
+    quantizer = vitis_quantize.VitisQuantizer(new_model, quantize_strategy='pof2s', custom_objects={'Length': Length, 'CapsuleLayer': CapsuleLayer, 'SquashLayer': SquashLayer})
+    quantized_model = quantizer.quantize_model(calib_dataset = x_test, add_shape_info = True, separate_conv_act = False)
+    quantized_model.save(os.path.join(save_path, 'quantized_partial_model.h5'))
 
-    # Quantizing
-    quantizer = vitis_quantize.VitisQuantizer(partial_model, quantize_strategy='pof2s', custom_objects={'Length': Length, 'CapsuleLayer': CapsuleLayer, 'SquashLayer': SquashLayer})
-    quantized_model = quantizer.quantize_model(calib_dataset=x_test, add_shape_info=True, separate_conv_act=False)
-
-    # Save the quantized partial model
-    quantized_model.save(os.path.join(save_path, 'no_digitcaps_model.h5'))
-
-    # Dump quantization results
-    vitis_quantize.VitisQuantizer.dump_model(model=quantized_model, dataset=x_test[0:1], output_dir="./dump_results", dump_float=True)
+    # dump quantize results
+    vitis_quantize.VitisQuantizer.dump_model( model=quantized_model, dataset=x_test[0:1], output_dir="./dump_results", dump_float=True)
 
     # (experimental) inspector code
     # inspector = vitis_inspect.VitisInspector(target="DPUCZDX8G_ISA1_B4096");
@@ -37,10 +31,10 @@ if __name__ == "__main__":
 
     # get user input on w
     parser = argparse.ArgumentParser(description="Run Vitis AI quantization")
-    parser.add_argument('--file_path', default=os.path.join('TF_models', 'no_reconstruction_eval_trained_model.h5'),
-                        help="Path of model to quantize. Default is TF_models/no_reconstruction_eval_model.h5")
-    parser.add_argument('--save_path', default='quantized_va',
-                        help="Path to save model. Default is quantized_va")
+    parser.add_argument('--file_path', default=os.path.join('models', 'partial_model.h5'),
+                        help="Path of model to quantize. Default is models/partial_model.h5")
+    parser.add_argument('--save_path', default='models/quant',
+                        help="Path to save model. Default is models/quant")
     args = parser.parse_args()
     print(args)
 
